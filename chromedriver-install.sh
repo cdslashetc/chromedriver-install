dcc73d9b17103f5dfa9546b5555b6bac207cd4de
Jonathan Dill <cdslashetc@cdslashetc.net>
2017-08-18 13:54:21 -0400

    Update chromedriver-install.sh

--- chromedriver-install.sh
+++ chromedriver-install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
+# take a snapshot of /usr/local in case we need to revert
+cd /usr
+d=$(date --iso-8601)
+tar cvf local-$d.tar
+pxz -v9 local-$d.tar
+# return to home
+cd ~
+apt-get update
 apt-get -y install libxss1 libappindicator1 libindicator7
 if [ ! -e /usr/bin/google-chrome ]; then
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -i google-chrome*.deb
   apt-get -y install -f
 fi
+apt-get -y install google-chrome
 apt-get -y install xvfb
 apt-get -y install unzip
 rm -f LATEST_RELEASE
